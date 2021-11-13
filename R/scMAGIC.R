89ae107198a096e19c0717ba5298425268f8d08f
drizzle-zhang <zhang_yu18@fudan.edu.cn>
2021-11-13 21:15:16 +0800

    update

--- R/scMAGIC.R
+++ R/scMAGIC.R
@@ -1333,7 +1333,11 @@ scMAGIC <- function(exp_sc_mat, exp_ref_mat, exp_ref_label = NULL,
         df.tags1 <- merge(df.tags1, df.dict, by = 'row.names')
         rownames(df.tags1) <- df.tags1$Row.names
         df.tags1$Row.names <- NULL
-        auc_gap <- (threshold-5) + 0.18
+        if (threshold <= 5) {
+            auc_gap <- (5-threshold)*2 + 0.2
+        } else {
+            auc_gap <- (5-threshold) + 0.2
+        }
         out.cutoff <- .cutoff_AUC(df.tags1, list_tags1_back, auc_gap, num_threads = num_threads)
         df.cutoff.1 <- out.cutoff$list.cutoff
         neg.cutoff.1 <- out.cutoff$vec.neg.cutoff
